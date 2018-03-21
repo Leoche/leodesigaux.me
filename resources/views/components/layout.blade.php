e7fa1f0cb6917b9636c8f8f83cede78bae638a02
Léo DESIGAUX <leodesigaux@gmail.com>
2018-03-21 21:35:02 +0100

    Fixed links on footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,35 +22,35 @@
         <div class="spacer is-large"></div>
         <p>N'hésitez pas à me contacter :)</p>
         <p class="has-text-grey">
-          Téléphone: <a href="#">+33 6 50 10 25 64</a><br/>
-          Email: <a href="#">leo.desigaux@gmail.com</a>
+          Téléphone: <a href="tel:+33650102564">+33 6 50 10 25 64</a><br/>
+          Email: <a href="mailto:leodesigaux@gmail.com">leo.desigaux@gmail.com</a>
         </p>
         <p class="icons is-clearfix">
-          <a href="#" class="link-twitter"><i class="ld ld-twitter"></i></a>
-          <a href="#" class="link-github"><i class="ld ld-github"></i></a>
-          <a href="#" class="link-soundcloud"><i class="ld ld-soundcloud"></i></a>
+          <a target="_blank" href="https://twitter.com/le0che" class="link-twitter"><i class="ld ld-twitter"></i></a>
+          <a target="_blank" href="https://github.com/leoche" class="link-github"><i class="ld ld-github"></i></a>
+          <a target="_blank" href="https://soundcloud.com/leodes" class="link-soundcloud"><i class="ld ld-soundcloud"></i></a>
         </p>
       </div>
       <div class="column">
         <h3><span>Navigation</span></h3>
         <ul>
-          <li><a href="#">Accueil</a></li>
-          <li><a href="#">Réalisations</a></li>
-          <li><a href="#">Labo</a></li>
-          <li><a href="#">Blog</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="/">Accueil</a></li>
+          <li><a href="/realisations">Réalisations</a></li>
+          <li><a href="/labo">Labo</a></li>
+          <li><a href="/blog">Blog</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
       </div>
       <div class="column">
         <h3><span>Informations</span></h3>
-        <p>Ce site web est sous license <a href="#">CC BY NC SA 4.0</a><br/>Son code source est disponible sur <a href="#">Github</a>.</p>
+        <p>Ce site web est sous license <a target="_blank" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a><br/>Son code source est disponible sur <a target="_blank" href="https://github.com/Leoche/leodesigaux.me">Github</a>.</p>
         <div class="spacer is-hidden-tablet"></div>
         <p class="credits">
-          <a href="ovh">
+          <a target="_blank" href="https://ovh.com">
             <span>Hébergé par</span>
             <img src="https://www.ovh.com/fr/news/logos/without-baseline/logo-ovh-sans-black-72DPI.png" alt="Hosted by Ovh">
           </a>
-          <a href="https://bulma.io">
+          <a target="_blank" href="https://bulma.io">
             <span>Réalisé avec</span>
             <img src="https://bulma.io/images/bulma-logo.png" alt="Made with Bulma">
           </a>
