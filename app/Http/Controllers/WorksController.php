f65aefcb98bfe64fe91f9bdd64f703d63f378b66
Léo DESIGAUX <leodesigaux@gmail.com>
2018-03-28 00:14:43 +0200

    Works enhancement

--- app/Http/Controllers/WorksController.php
+++ app/Http/Controllers/WorksController.php
@@ -11,7 +11,20 @@ class WorksController extends ContentfulController
 
     $entries = $this->client->getEntries($this->query);
     setlocale (LC_TIME, "fr_FR.utf8");
-    return view("realisations")->with("entries", $entries);
+    $content = array();
+    $titles = array(
+      "web"=>"Projets web",
+      "game"=>"Jeux vidéo",
+      "other"=>"Logiciels & autres"
+    );
+    foreach ($entries->getItems() as $entry) {
+      if (isset($content[$entry->getCategory()])) {
+        array_push($content[$entry->getCategory()], $entry);
+      } else {
+        $content[$entry->getCategory()] = [$entry];
+      }
+    }
+    return view("realisations")->with("content", $content)->with("titles", $titles);
   }
 
 }
--- resources/views/realisations.blade.php
+++ resources/views/realisations.blade.php
@@ -2,56 +2,73 @@
 @section('content')
 <section>
   <div class="container is-padded-mobile">
-    <div class="content">
-      <h1>Réalisations</h1>
-      <hr>
-    </div>
-    <div class="column-grid column-tri">
-      @foreach(array_reverse($entries->getItems()) as $entry)
-      <div class="card card-work">
-        <div class="card-image">
-          <figure class="image is-16by9">
-            <img src="{{ $entry->getPreview()->getFile()->getUrl() }}" alt="Placeholder image">
-          </figure>
-        </div>
-        <div class="card-content">
-          <div class="level is-mobile">
-            <div class="level-left">
-              <div class="content">
-                <div class="title">{{ $entry->getTitle() }}</div>
-                <div class="subtitle">{{ ucwords(strftime('%B %G', $entry->getDate()->getTimestamp())) }}</div>
-              </div>
-            </div>
-            <div class="level-right">
-              <i class="ld ld-monitor"></i>
-              @if($entry->getResponsive())
-              <i class="ld ld-smartphone"></i>
-              @endif
-            </div>
-          </div>
-          <div class="content">
-            <p>{{ $entry->getDescription() }}</p>
-          </div>
-        </div>
-        <div class="card-footer">
-          <div class="level is-mobile">
-            <div class="level-left">
-              <div class="level-item">
-                <a target="_blank" href="{{ $entry->getLink() }}">VISITER <i class="ld ld-chevrons-right"></i></a>
-              </div>
-            </div>
-            <div class="level-right">
-              <div class="level-item tag-container">
-              @foreach($entry->getTags() as $tags)
-              <span>{{ $tags }}</span>
-              @endforeach
-              </div>
-            </div>
-          </div>
-        </div>
+    <div class="level is-pagetitle is-mobile">
+      <div class="level-left content">
+        <h1>Réalisations</h1>
       </div>
-      @endforeach
+      <div class="level-right">
+        <a class="toggle" href="#"><i class="ld ld-filter"></i> Filtres</a>
+        <p class="filters">
+          <span>:</span>
+          @foreach($content as $category => $entries)
+          <span class="tag" data-target="{{ $category }}">{{ $titles[$category] }}</span>
+          @endforeach
+        </p>
+      </div>
     </div>
+    @foreach($content as $category => $entries)
+    <div class="filtered" data-target="{{ $category }}">
+      <div class="content">
+        <h2><span>Coordonnées</span></h2>
+      </div>
+      <div class="column-grid column-tri">
+        @foreach(array_reverse($entries) as $entry)
+        <div class="card card-work">
+          <div class="card-image">
+            <figure class="image is-16by9">
+              <img src="{{ $entry->getPreview()->getFile()->getUrl() }}" alt="Placeholder image">
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="level is-mobile">
+              <div class="level-left">
+                <div class="content">
+                  <div class="title">{{ $entry->getTitle() }}</div>
+                  <div class="subtitle">{{ ucwords(strftime('%B %G', $entry->getDate()->getTimestamp())) }}</div>
+                </div>
+              </div>
+              <div class="level-right">
+                <i class="ld ld-monitor"></i>
+                @if($entry->getResponsive())
+                <i class="ld ld-smartphone"></i>
+                @endif
+              </div>
+            </div>
+            <div class="content">
+              <p>{{ $entry->getDescription() }}</p>
+            </div>
+          </div>
+          <div class="card-footer">
+            <div class="level is-mobile">
+              <div class="level-left">
+                <div class="level-item">
+                  <a target="_blank" href="{{ $entry->getLink() }}">VISITER <i class="ld ld-chevrons-right"></i></a>
+                </div>
+              </div>
+              <div class="level-right">
+                <div class="level-item tag-container">
+                  @foreach($entry->getTags() as $tags)
+                  <span>{{ $tags }}</span>
+                  @endforeach
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        @endforeach
+      </div>
+    </div>
+    @endforeach
   </div>
 </section>
 @endsection
